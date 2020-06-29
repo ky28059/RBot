9bd423974768a857d13bfb78d60f51ba9648f8cc
ky28059 <kevinyu567@gmail.com>
2020-06-29 15:15:40 -0700

    More features!
    -wiseguy-esque responding to the exact phrase 'Incorrect.'
    
    -a very badly coded !say command

--- bot.js
+++ bot.js
@@ -20,12 +20,20 @@ bot.on('ready', function (evt) {
   logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
+  // Responses that are not commands
+  if (message == 'Incorrect.') {
+    bot.sendMessage({
+      to: channelID,
+      message: 'Misleading and Wrong.'
+    });
+  }
+
   // Bot listens to messages with the prefix !
   if (message.substring(0, 1) == '!') {
     var args = message.substring(1).split(' ');
     var cmd = args[0];
 
-    args = args.splice(1);
+    args = args.splice(1); //removes cmd from the array, making the array contain only arguments
     // Commands
     switch (cmd) {
       case 'ping':
@@ -34,19 +42,39 @@ bot.on('message', function (user, userID, channelID, message, evt) {
           message: '<@!' + userID + '>'
         });
         break;
+
+      case 'say':
+        if (args.length > 0) {
+          let msg = '';
+          for (var i = 0; i < args.length; i++) {
+            msg = msg + args[i] + ' ';
+          }
+          bot.sendMessage({
+            to: channelID,
+            message: msg
+          });
+        } else {
+          bot.sendMessage({
+            to: channelID,
+            message: 'You must specify what to say!'
+          });
+        }
+        break;
+
       case 'arugula':
         bot.sendMessage({
           to: channelID,
           message: 'Broccoli'
         });
         break;
+
       case 'help':
         bot.sendMessage({
           to: channelID,
-          message: 'My commands are: \n **!ping:** pings you \n **!arugula:** funny broccoli haha'
+          message: 'My commands are: \n **!ping:** pings you \n **!say:** says what you tell it to say \n **!arugula:** funny broccoli haha'
         });
         break;
-      /* 
+      /*
       default:
         bot.sendMessage({
           to: channelID,
