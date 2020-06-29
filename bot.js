9b3990732bedb4262f99d881f642aa750397aab7
ky28059 <kevinyu567@gmail.com>
2020-06-29 16:29:55 -0700

    Patched !say !say
    Now bot ignores itself when looking for commands

--- bot.js
+++ bot.js
@@ -20,67 +20,69 @@ bot.on('ready', function (evt) {
   logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-  // Responses that are not commands
-  if (message == 'Incorrect.') {
-    bot.sendMessage({
-      to: channelID,
-      message: 'Misleading and Wrong.'
-    });
-  }
+  if (userID != '684587440777986090') {
+    // Responses that are not commands
+    if (message == 'Incorrect.') {
+      bot.sendMessage({
+        to: channelID,
+        message: 'Misleading and Wrong.'
+      });
+    }
 
-  // Bot listens to messages with the prefix !
-  if (message.substring(0, 1) == '!') {
-    var args = message.substring(1).split(' ');
-    var cmd = args[0];
+    // Bot listens to messages with the prefix !
+    if (message.substring(0, 1) == '!') {
+      var args = message.substring(1).split(' ');
+      var cmd = args[0];
 
-    args = args.splice(1); //removes cmd from the array, making the array contain only arguments
-    // Commands
-    switch (cmd) {
-      case 'ping':
-        bot.sendMessage({
-          to: channelID,
-          message: '<@!' + userID + '>'
-        });
-        break;
+      args = args.splice(1); //removes cmd from the array, making the array contain only arguments
+      // Commands
+      switch (cmd) {
+        case 'ping':
+          bot.sendMessage({
+            to: channelID,
+            message: '<@!' + userID + '>'
+          });
+          break;
 
-      case 'say':
-        if (args.length > 0) {
-          let msg = '';
-          for (var i = 0; i < args.length; i++) {
-            msg = msg + args[i] + ' ';
+        case 'say':
+          if (args.length > 0) {
+            let msg = '';
+            for (var i = 0; i < args.length; i++) {
+              msg = msg + args[i] + ' ';
+            }
+            bot.sendMessage({
+              to: channelID,
+              message: msg
+            });
+          } else {
+            bot.sendMessage({
+              to: channelID,
+              message: 'You must specify what to say!'
+            });
           }
+          break;
+
+        case 'arugula':
           bot.sendMessage({
             to: channelID,
-            message: msg
+            message: 'Broccoli'
           });
-        } else {
+          break;
+
+        case 'help':
           bot.sendMessage({
             to: channelID,
-            message: 'You must specify what to say!'
+            message: 'My commands are: \n **!ping:** pings you \n **!say:** says what you tell it to say \n **!arugula:** funny broccoli haha'
           });
-        }
-        break;
-
-      case 'arugula':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Broccoli'
-        });
-        break;
-
-      case 'help':
-        bot.sendMessage({
-          to: channelID,
-          message: 'My commands are: \n **!ping:** pings you \n **!say:** says what you tell it to say \n **!arugula:** funny broccoli haha'
-        });
-        break;
-      /*
-      default:
-        bot.sendMessage({
-          to: channelID,
-          message: 'Sorry, I didn\'t seem to get that.'
-        });
-      */
-     }
-   }
+          break;
+        /*
+        default:
+          bot.sendMessage({
+            to: channelID,
+            message: 'Sorry, I didn\'t seem to get that.'
+          });
+        */
+      }
+    }
+  }
 });
