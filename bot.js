db409a47dc7ea5319b70ac17ffbe97d6a766e800
ky28059 <kevinyu567@gmail.com>
2020-07-11 19:39:46 -0700

    Switch to discord.js
    rip discord.io

--- bot.js
+++ bot.js
@@ -1,36 +1,23 @@
-const Discord = require('discord.io');
-const logger = require('winston');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const auth = require('./auth.json');
-let isStreaming = false;
-
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-  colorize: true
-});
-logger.level = 'debug';
+//let isStreaming = false;
 
 // Initialize Discord Bot
-const bot = new Discord.Client({
-  token: auth.token,
-  autorun: true
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  //bot.setPresence({game: {name: '!help'}}); // sets status
 });
-bot.on('ready', function (evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
-  bot.setPresence({game: {name: '!help'}}); // sets status
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-  if (userID != '684587440777986090') { // Bot ignores itself
+client.on('message', message => {
+  if (message.author.id != '684587440777986090') { // Bot ignores itself
     // Responses that are not commands
-    if (message.toLowerCase().includes('incorrect')) { // toLowerCase makes this non case sensitive
-      bot.sendMessage({to: channelID, message: 'Misleading and Wrong.'}); // maybe too spammy?
+    if (message.content.toLowerCase().includes('incorrect')) { // toLowerCase makes this non case sensitive
+      message.channel.send('Misleading and Wrong.'); // maybe too spammy?
     }
 
     // Bot listens to messages with the prefix !
-    if (message.substring(0, 1) == '!') {
-      var args = message.substring(1).split(' ');
+    if (message.content.substring(0, 1) == '!') {
+      var args = message.content.substring(1).split(' ');
       var cmd = args[0];
 
       args = args.splice(1); // removes cmd from the array, making the array contain only arguments to commands
@@ -38,7 +25,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       switch (cmd) {
         case 'ping':
           let ping = Date.now() - message.createdTimestamp; // returns NaN, still broken
-          bot.sendMessage({to: channelID, message: '<@!' + userID + '>, your ping is `' + ping + ' ms`'});
+          message.channel.send('<@!' + message.author.id + '>, your ping is `' + ping + ' ms`');
           break;
 
         case 'say':
@@ -47,12 +34,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             for (let i = 0; i < args.length; i++) {
               msg = msg + args[i] + ' ';
             }
-            bot.sendMessage({to: channelID, message: msg});
+            message.channel.send(msg);
           } else {
-            bot.sendMessage({to: channelID, message: 'You must specify what to say!'});
+            message.channel.send('You must specify what to say!');
           }
           break;
 
+        /*
         case 'stream': // streaming lol
           if (args.length >= 1) { // doesn't check whether bot is already streaming so !stream can be used to update what the bot is streaming, not just to toggle
             let msg = args[0];
@@ -63,34 +51,37 @@ bot.on('message', function (user, userID, channelID, message, evt) {
               }
             }
             bot.setPresence({game: {name: msg, type: 1, url: 'https://www.twitch.tv/' + args[0]}});
-            bot.sendMessage({to: channelID, message: 'Now streaming `https://www.twitch.tv/' + args[0] + '` with the message `' + msg + '`!'});
+            message.channel.send('Now streaming `https://www.twitch.tv/' + args[0] + '` with the message `' + msg + '`!');
             if (!isStreaming) {
               isStreaming = true;
             }
           } else {
             if (!isStreaming) {
               bot.setPresence({game: {name: 'whatever Amir\'s doing', type: 1, url: 'https://www.twitch.tv/speed__ow'}}); // default stream is Amir's twitch
-              bot.sendMessage({to: channelID, message: 'Now streaming!'});
+              message.channel.send('Now streaming!');
             } else {
               bot.setPresence({game: {name: '!help'}});
-              bot.sendMessage({to: channelID, message: 'Stopped streaming!'});
+              message.channel.send('Stopped streaming!');
             }
             isStreaming = !isStreaming;
           }
           break;
+          */
 
         case 'arugula':
-          bot.sendMessage({to: channelID, message: 'Broccoli'});
+          message.channel.send('Broccoli');
           break;
 
         case 'help':
-          bot.sendMessage({to: channelID, message: 'My commands are: \n **!ping:** pings you \n **!say [message]:** says what you tell it to say \n **!stream [twitch name] [displayed status]:** tells the bot to stream twitch.tv/[twitch name] with [displayed status]; if no argument is provided it defaults to plugging Amir\'s twitch \n **!arugula:** funny broccoli haha \n I also respond to anyone who dares use the word `incorrect` in their sentence.'});
+          message.channel.send({to: channelID, message: 'My commands are: \n **!ping:** pings you \n **!say [message]:** says what you tell it to say \n **!stream [twitch name] [displayed status]:** tells the bot to stream twitch.tv/[twitch name] with [displayed status]; if no argument is provided it defaults to plugging Amir\'s twitch \n **!arugula:** funny broccoli haha \n I also respond to anyone who dares use the word `incorrect` in their sentence.'});
           break;
         /*
         default:
-          bot.sendMessage({to: channelID, message: 'Sorry, I didn\'t seem to get that.'});
+          message.channel.send({to: channelID, message: 'Sorry, I didn\'t seem to get that.'});
         */
       }
     }
   }
 });
+
+client.login(auth.token);
