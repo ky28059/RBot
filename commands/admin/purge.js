b03d063769b4d862adc96b2d6c0296c7992d8b9e
Kevin Yu <kevinyu567@gmail.com>
2020-10-22 01:55:55 +0000

    Support a less good version of purge by user

--- commands/admin/purge.js
+++ commands/admin/purge.js
@@ -1,8 +1,8 @@
 export default {
     name: 'purge',
     description: 'Bulk deletes the specified amount of messages in the channel.',
-    usage: 'purge [1-99]',
-    examples: 'purge 80',
+    usage: ['purge [1-99]', 'purge [1-99] @[user]'],
+    examples: ['purge 80', 'purge 80 @RBot'],
     guildOnly: true,
     permReqs: 'MANAGE_MESSAGES',
     clientPermReqs: 'MANAGE_MESSAGES',
@@ -10,7 +10,10 @@ export default {
         let deleteCount = Number(parsed.first);
         if (!deleteCount || deleteCount < 1 || deleteCount > 99) return message.reply("please provide a number between 1 and 99 for the number of messages to delete");
 
-        const fetched = await message.channel.messages.fetch({limit: deleteCount + 1});
+        let fetched = await message.channel.messages.fetch({limit: deleteCount + 1});
+        if (parsed.memberTarget) // Supports purge by user
+            fetched = fetched.filter(message => message.author.id === parsed.memberTarget.id);
+
         message.channel.bulkDelete(fetched)
             .catch(error => message.reply(`couldn't delete messages because of: ${error}`));
     }
