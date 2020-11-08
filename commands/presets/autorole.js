b7b0eae9b0d70d758fd82b7b2dfad376a90bea8c
Kevin Yu <kevinyu567@gmail.com>
2020-11-08 03:17:41 +0000

    Add removing from blacklist, make autorole log

--- commands/presets/autorole.js
+++ commands/presets/autorole.js
@@ -1,3 +1,6 @@
+import {isInField, addToField, removeFromField} from '../utils/tokenFieldManager.js';
+import {log} from "../utils/logger.js";
+
 export default {
     name: 'autorole',
     description: 'Adds / removes roles from the autorole (to be added to new members upon join).',
@@ -13,26 +16,30 @@ export default {
 
         if (!roleTarget) return message.reply('you must specify a role to add/remove from autorole!');
         if (!roleTarget.editable) return message.reply('that role is too high up in the hierarchy! Please adjust it so that my highest role is above that role!');
-        if (!action) return message.reply('you must specify what to do with that role!');
+        if (!action) return message.reply('you must specify an action to perform with that role!');
 
         const tag = await client.Tags.findOne({ where: { guildID: guild.id } });
+        let messageArg;
 
         switch (action) {
             case 'add':
-                if (tag.autoroles && tag.autoroles.includes(roleTarget.id)) return message.reply("that role has already been added to autorole!");
-                tag.autoroles += roleTarget.id + ' ';
+                if (isInField(tag, 'autoroles', roleTarget.id)) return message.reply("that role is already in autorole!");
+                await addToField(tag, 'autoroles', roleTarget.id);
+                messageArg = 'added to';
                 break;
 
             case 'remove':
-                if (tag.autoroles && !tag.autoroles.includes(roleTarget.id)) return message.reply("that role has not been added to autorole!");
-                tag.autoroles = tag.autoroles.replace(`${roleTarget.id} `, '');
+                if (!isInField(tag, 'autoroles', roleTarget.id)) return message.reply("that role is not in autorole!");
+                await removeFromField(tag, 'autoroles', roleTarget.id);
+                messageArg = 'removed from';
                 break;
 
             default:
                 return message.reply(`${action} is not a valid action!`);
         }
-        // TODO: log this
-        await tag.save();
-        message.channel.send(`Success! Autorole has been updated!`);
+
+        await log(client, guild, 0xf6b40c, message.author.tag, message.author.avatarURL(),
+            `**${roleTarget} has been ${messageArg} this server's autorole by ${message.author} in ${message.channel}**\n[Jump to message](${message.url})`);
+        message.channel.send(`${roleTarget.id} has been ${messageArg} this server's autorole!`);
     }
 }
---
+++ commands/presets/blacklist.js
@@ -0,0 +1,46 @@
+import {isInField, addToField, removeFromField} from '../utils/tokenFieldManager.js';
+import {log} from "../utils/logger.js";
+
+export default {
+    name: 'blacklist',
+    aliases: ['hackban'],
+    description: 'Add or remove a user from the server blacklist (blacklisted users are banned upon joining).',
+    usage: ['blacklist add @[user]', 'blacklist remove @[user]'],
+    examples: ['blacklist add @example', 'blacklist remove @example'],
+    guildOnly: true,
+    permReqs: 'BAN_MEMBERS',
+    clientPermReqs: 'BAN_MEMBERS',
+    async execute(message, parsed, client) {
+        const guild = message.guild;
+        const action = parsed.first;
+        const userTarget = parsed.userTarget;
+
+        if (!userTarget) return message.reply("please mention a valid user id");
+        if (userTarget.id === message.author.id) return message.reply("you cannot blacklist yourself!");
+        if (!action) return message.reply('you must specify an action to perform with that user!');
+
+        const tag = await client.Tags.findOne({ where: { guildID: guild.id } });
+        let messageArg;
+
+        switch (action) {
+            case 'add':
+                if (isInField(tag, 'blacklist', userTarget.id)) return message.reply("that user is already blacklisted!");
+                await addToField(tag, 'blacklist', userTarget.id);
+                messageArg = 'added to';
+                break;
+
+            case 'remove':
+                if (!isInField(tag, 'blacklist', userTarget.id)) return message.reply("that user is not blacklisted!");
+                await removeFromField(tag, 'blacklist', userTarget.id);
+                messageArg = 'removed from';
+                break;
+
+            default:
+                return message.reply(`${action} is not a valid action!`);
+        }
+
+        await log(client, guild, 0x7f0000, userTarget.tag, userTarget.avatarURL(),
+            `**${userTarget} has been ${messageArg} this server's blacklist by ${message.author} in ${message.channel}**\n[Jump to message](${message.url})`);
+        message.channel.send(`${userTarget.tag} has been ${messageArg} this server's blacklist!`);
+    }
+}
