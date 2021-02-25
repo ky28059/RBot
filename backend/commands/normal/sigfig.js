66ddf561946f5c1216a6df07a2e6c946b77f559d
Kevin Yu <kevinyu567@gmail.com>
2021-02-24 20:47:18 -0800

    Add !sigfig

---
+++ backend/commands/normal/sigfig.js
@@ -0,0 +1,51 @@
+import IllegalArgumentError from '../../errors/IllegalArgumentError.js';
+
+
+export default {
+    name: 'sigfig',
+    description: 'gets the sigfig info of a given number.',
+    pattern: '[Number]',
+    examples: ['sigfig 5.0', 'sigfig 32.582736'],
+    execute(message, parsed) {
+        let { number } = parsed;
+
+        if (isNaN(number))
+            throw new IllegalArgumentError(this.name, 'Field `Number` must be a valid number');
+        if (Number(number) > Number.MAX_SAFE_INTEGER)
+            throw new IllegalArgumentError(this.name, 'Field `Number` cannot exceed the JS maximum safe integer');
+
+        // Funky looking conversion to convert scientific notation to regular
+        let num = String(Number(number));
+
+        let [whole, dec] = num.split('.');
+        if (dec !== undefined) { // If a decimal point exists
+            return message.channel.send(`**${num}**, ${num.length - 1} significant figure(s)`);
+        }
+
+        let {text, significant} = parseTrailingZeroes(num);
+        message.channel.send(`${text}, ${significant} significant figure(s)`);
+    }
+}
+
+const parseTrailingZeroes = (num) => {
+    let temp = Number(num);
+    let numTrailing = 0;
+
+    // Base case for 0 to prevent infinite loop
+    if (temp === 0) return {
+        text: `${num}`,
+        significant: 0
+    };
+
+    // Loop until all trailing zeros have been sliced
+    while (temp % 10 === 0) {
+        temp /= 10;
+        numTrailing++;
+    }
+
+    let significant = num.length - numTrailing;
+    return {
+        text: `**${num.slice(0, significant)}**${num.slice(significant)}`,
+        significant: significant
+    };
+}
