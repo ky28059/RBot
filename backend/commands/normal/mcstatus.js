78bba0d18591b8ecb0e2949be7f94a5de68ccbdb
Kevin Yu <kevinyu567@gmail.com>
2021-06-08 10:58:03 -0700

    Make mcstatus handle nonexistent servers, make embed look prettier

--- backend/commands/normal/mcstatus.js
+++ backend/commands/normal/mcstatus.js
@@ -12,14 +12,19 @@ export default {
         const server = parsed.serverip;
         const source = await (await fetch(`https://mcsrvstat.us/server/${server}`)).text();
 
-        const players = source.match(/<td>Players<\/td>\s+<td>\s(.+)(?: - <a href="#" id="show_players"|<\/td>)/)[1];
-        const version = source.match(/<td>Version<\/td>\s+<td>\s(.+)<\/td>/)[1];
+        const players = source.match(/<td>Players<\/td>\s+<td>\s(.+)(?: - <a href="#" id="show_players"|<\/td>)/)?.[1];
+        const version = source.match(/<td>Version<\/td>\s+<td>\s(.+)<\/td>/)?.[1];
 
         const serverEmbed = new MessageEmbed()
-            .setColor(0x333333)
+            .setColor(0x333333);
+
+        if (!players || !version) serverEmbed
+            .setAuthor('Server not found.')
+        else serverEmbed
             .setAuthor(server)
             .setDescription(`**Players:** ${players}\n**Version:** ${version}`)
             .setFooter(`Requested by ${message.author.tag}`);
+
         message.channel.send(serverEmbed);
     }
 }
