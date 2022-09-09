e43b6f7f12ccd83cbcdd3724af4c25df96169481
Kevin Yu <kevinyu567@gmail.com>
2022-09-08 21:05:02 -0700

    Create `/invite`

---
+++ commands/normal/invite.ts
@@ -0,0 +1,26 @@
+import {createSlashCommand} from '../../utils/commands';
+import {OAuth2Scopes, PermissionsBitField} from 'discord.js';
+import {SlashCommandBuilder} from '@discordjs/builders';
+import {replyEmbed} from '../../utils/messageUtils';
+import {success} from '../../utils/messages';
+
+
+export const data = new SlashCommandBuilder()
+    .setName('invite')
+    .setDescription('Sends RBot\'s invite link.')
+
+export default createSlashCommand({
+    data,
+    async execute(message) {
+        const link = message.client.generateInvite({
+            permissions: [PermissionsBitField.Flags.Administrator],
+            scopes: [OAuth2Scopes.Bot, OAuth2Scopes.ApplicationsCommands]
+        });
+
+        const inviteEmbed = success()
+            .setAuthor({name: 'Invite link:'})
+            .setDescription(`[Click here to invite RBot to another server!](${link})`);
+
+        await replyEmbed(message, inviteEmbed);
+    }
+});
